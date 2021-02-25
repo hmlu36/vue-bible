5019005d686c5d564c0128dc2641605bcd17f789
joshua.lu <hmlu36@gmail.com>
2021-02-25 09:48:08 +0000

    deploy to Github pages

---
+++ precache-manifest.ba3c6a27cca1e970b5e9088ea005b095.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4644b96174c7d77a0df",
+    "url": "/vue-bible/css/app.c8231583.css"
+  },
+  {
+    "revision": "e072bb6fbfe26ca37c63",
+    "url": "/vue-bible/css/chunk-vendors.b6deeff1.css"
+  },
+  {
+    "revision": "861985606429972325c9352b213b532b",
+    "url": "/vue-bible/index.html"
+  },
+  {
+    "revision": "d4644b96174c7d77a0df",
+    "url": "/vue-bible/js/app.19ca0149.js"
+  },
+  {
+    "revision": "e072bb6fbfe26ca37c63",
+    "url": "/vue-bible/js/chunk-vendors.49fb5f3a.js"
+  },
+  {
+    "revision": "e57ad913ee47aff17c310260dce111c4",
+    "url": "/vue-bible/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-bible/robots.txt"
+  },
+  {
+    "revision": "1ef6e4ece62cd6d4ff7cdf4ab7f4191d",
+    "url": "/vue-bible/template/clerical-template.docx"
+  },
+  {
+    "revision": "982dd43bec822d5811dca96038792dc7",
+    "url": "/vue-bible/template/regular-template.docx"
+  },
+  {
+    "revision": "f050abb8a278e1013be83c8c7659f2c6",
+    "url": "/vue-bible/template/seal-character-template.docx"
+  },
+  {
+    "revision": "6197e9aca481a6c4bf3653e3ddbeb85d",
+    "url": "/vue-bible/template/semi-cursive-template.docx"
+  }
+]);
